84832d439b3b0af5be75a6f656f4b2ba2ec4304b
Phraymus <90783101+Phraymus@users.noreply.github.com>
2022-03-18 16:58:19 -0500

    Se agrego carpeta Controller y Logic

---
+++ src/java/modelo/logic/PersonaLogic.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.logic;
+
+import java.util.ArrayList;
+import modelo.beans.Persona;
+import modelo.dao.PersonaDAO;
+import modelo.interfaces.PersonaInterface;
+
+/**
+ *
+ * @author Brayan
+ */
+public class PersonaLogic implements PersonaInterface{
+
+    PersonaDAO personaDao= new PersonaDAO();
+    @Override
+    public ArrayList<Persona> listar() {
+        return personaDao.listar();
+    }
+
+    @Override
+    public Persona buscar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean insertar(Persona persona) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean editar(Persona persona) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean eliminar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
