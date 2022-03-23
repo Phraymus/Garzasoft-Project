6ba68ad65fdae8be2c7dd8682b938c071c9dee35
Sttefany07 <99628093+Sttefany07@users.noreply.github.com>
2022-03-22 21:31:44 -0500

    proyectoLogic

---
+++ src/java/modelo/logic/ProyectoLogic.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo.logic;
+
+import java.util.ArrayList;
+import modelo.beans.Proyecto;
+import modelo.interfaces.ProyectoInterface;
+
+/**
+ *
+ * @author Windows10
+ */
+public class ProyectoLogic implements ProyectoInterface {
+
+    @Override
+    public ArrayList<Proyecto> listar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Proyecto buscar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean insertar(Proyecto proyecto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean editar(Proyecto proyecto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean eliminar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
